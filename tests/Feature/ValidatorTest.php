e75d3757f0cdf2b23c74535c52f0c5b786b43b1b
azdyf <azdy.fm@google.com>
2025-06-29 11:58:59 +0700

    Validation Exception

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
@@ -50,6 +51,33 @@ class ValidatorTest extends TestCase
         self::assertIsArray($message->toArray());
         self::assertJson($message->toJson());
         Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidationException()
+    {
+        $data = [
+            'username' => '',
+            'password' => '',
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        try {
+            $validator->validate();
+            self::fail('ValidationException was not thrown');
+        } catch (ValidationException $exception) {
+            self::assertNotNull($exception->validator);
+//            $message = $exception->errors(); // OR
+//            $message = $exception->validator->getMessageBag(); // OR
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
     }
 
 }
