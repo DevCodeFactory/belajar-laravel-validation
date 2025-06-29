4dd61b1cb19346b2e2d37d92ec2c4cdb9c29c9b6
azdyf <azdy.fm@google.com>
2025-06-29 11:47:57 +0700

    Error Message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -2,8 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -45,6 +44,12 @@ class ValidatorTest extends TestCase
         self::assertNotNull($validator);
         self::assertFalse($validator->passes());
         self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        self::assertIsArray($message->toArray());
+        self::assertJson($message->toJson());
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 
 }
